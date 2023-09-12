c9d05f5045969fca2fc86f9db8e7a38b6a376a8a
unknown <hiurimi@gmail.com>
2023-09-12 14:02:30 -0400

    magic question

--- server.js
+++ server.js
@@ -23,6 +23,19 @@ app.get('/tip/:total/:tipPercentage', (req, res) =>{
 })
 
 
+/*Magic 8 */
+app.get('/magic/:question', (req, res) => {
+    const question = req.params.question.replace(/%20/g," ")
+    const magic8BallResponses = 
+        ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    ;
+
+    const randIdx = Math.floor(Math.random( )*20);
+
+    res.send(`<h1>${question} ? ${magic8BallResponses[randIdx]}</h1>` )
+});
+
+
 
 
 app.listen(port, () => {
